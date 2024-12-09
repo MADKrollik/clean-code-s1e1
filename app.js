423cb2d3f4125a89de7f96e9948ad4fa24cd2fec
Ihor Salfetnyk <4el@ukr.net>
2024-12-09 21:01:57 +0200

    refactor(extended-1.2) alt for multimedia

--- app.js
+++ app.js
@@ -48,6 +48,7 @@ var createNewTaskElement=function(taskString){
   deleteButton.className="button button-delete";
   deleteButtonImg.src="./remove.svg";
   deleteButtonImg.className="remove-img";
+  deleteButtonImg.alt="";
   deleteButton.appendChild(deleteButtonImg);
 
 
